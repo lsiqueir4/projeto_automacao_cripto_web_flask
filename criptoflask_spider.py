dca123da84839aae922f10df21b61b64d8b5fe6e
Leandro Siqueira <link_le@hotmail.com>
2022-01-22 18:32:05 -0300

    patch 1
    Arquivos desnecessários da spider excluidos, erro 403 resolvido com inclusao do custom settings

--- criptoflask_spider.py
+++ criptoflask_spider.py
@@ -1,4 +1,4 @@
-from urllib.request import Request
+#from urllib.request import Request
 import scrapy
 import time
 
@@ -11,13 +11,21 @@ links = ['bombcrypto',
         ]
 
 class CriptoflaskSpiderSpider(scrapy.Spider):
+    custom_settings = {
+        'DOWNLOADER_MIDDLEWARES' : {
+                'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+                'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
+            }
+    }
+    
     name = 'criptoflask_spider'
-    start_urls = [f'https://coinmarketcap.com/pt-br/currencies/{i}/' for i in links]
+    start_urls = [f'https://coinmarketcap.com/pt-br/currencies/{i}/' for i in links ]
+    
     
     def parse(self, response):
-           yield {
+        yield {
             'nome' : response.css('.h1 ::text').get(),
             'sigla' : response.css('.nameSymbol ::text').get(),
             'cotacao' : response.css('.priceValue span ::text').get()
             }
-        time.sleep(4)
+        
