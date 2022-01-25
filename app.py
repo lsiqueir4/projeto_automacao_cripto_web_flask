d5759b4f9d50154fb43d9ed3e81297a05cc79901
Leandro Siqueira <link_le@hotmail.com>
2022-01-25 10:26:17 -0300

    patch 4
    Inserida a variável lista_filtrada, onde são armazenados apenas os resultados solicitados pela checkbox, necessário apresentar os resultados na pagina através de um for no html da pag. Também foram adicionados alguns comentários.

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import os, time, scrapy, json
 
 app = Flask(__name__)
 
-cripto_lista = []
+cripto_lista = [] #list dos checkbox
 
-lista_dicionario = []
+lista_dicionario = [] #lista de cotacoes coletadas
+
+lista_filtrada = [] #lista com os filtros do checkbox
 
 #Chamada da Spider para coletar as cotações
 def chamar_spider():
@@ -20,12 +22,13 @@ def chamar_spider():
 #Função para pegar o JSON criado, 
 # armazená-lo em uma variavel e depois excluí-lo
 def pegar_json():
+    #ADD COTACOES DO JSON NA VARIAVEL lista_dicionario
     try:
         with open ('cotacao.json') as json_file:
             cotacoes = json.load(json_file)
         time.sleep(2)
         for cotacao in cotacoes:
-            lista_dicionario.append(cotacao)
+            lista_dicionario.append(cotacao) 
         print('JSON COLETADO COM SUCESSO!')
         #EXCLUINDO JSON
         try:
@@ -35,6 +38,13 @@ def pegar_json():
             print('NÃO FOI POSSIVEL EXCLUIR O JSON')
     except ValueError as v:
         print(f"ERRO! COLETA DO ARQUIVO JSON FALHOU, ERRO:{v}")
+
+#FILTRANDO A LISTA COM OS DADOS DO CHECKBOX
+def filtrar_cotacoes():
+    for cotacoes in lista_dicionario:
+        if cotacoes['nome'] in cripto_lista:
+            lista_filtrada.append(cotacoes)
+    print(f'LISTA FILTRADA:{lista_filtrada}')
 
 #home
 @app.route('/', methods = ['GET', 'POST'])
@@ -48,28 +58,28 @@ def home():
     #pegando os dados do checkbox como list
     if request.method == 'POST':
         data = request.form.getlist('criptomoedas')
-        for i in data:
+        for i in data: #for para inserir dados do checkbox na list
             cripto_lista.append(i) 
-        print(cripto_lista)
-        if not cripto_lista:
-            erro = 'Você não selecionou nenhuma criptomoeda!'
+        print(cripto_lista) 
+        if not cripto_lista: #Se a lista está vazia
+            pass
         else:
-            flash('Estamos coletando suas cotações')
             chamar_spider()
             time.sleep(2)
             pegar_json()
-            print(lista_dicionario)
+            print(lista_dicionario) #cotacoes coletadas
             time.sleep(2)
+            filtrar_cotacoes()
             return redirect('/cotacoes/')
-        return render_template('home.html', error=erro) 
+        return render_template('home.html') 
 
 #Pagina das cotações coletadas
 @app.route('/cotacoes/', methods = ['GET'])
 def pagina_cotacoes():
-    for i in lista_dicionario:
-        print (i)
-        return i
-
+    return render_template('cotacoes.html')
+    
+    
+         
 if __name__ == '__main__':
     #rodando o web
     app.run(debug=True)
